69d7ccee838f947483f4e9dff05d789d57e673b1
tobiaszpiwowarczyk <tobiasz_piwowarczyk@outlook.com>
2018-07-27 14:42:27 +0200

    - added scope settings support

--- frontend/src/app/modules/home/components/unit/unit.component.ts
+++ frontend/src/app/modules/home/components/unit/unit.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Unit } from './Unit';
 
 @Component({
@@ -10,7 +10,22 @@ export class UnitComponent implements OnInit {
 
   @Input() unit: Unit;
 
+  options: number[] = [15, 20, 25, 30];
+  selected: number = 20;
+  optionsShown: boolean = false;
+
   constructor() {}
   ngOnInit() {}
 
+  public toggleOptions(): void {
+    this.optionsShown = !this.optionsShown;
+  }
+
+  public selectOption(option: number): void {
+    this.selected = option;
+  }
+
+  public saveScope(): void {
+    localStorage.setItem("scope", this.selected.toString());
+  }
 }
