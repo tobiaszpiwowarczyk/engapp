9fea1ae5bcc5ba86ead924a00a2e013d85398daa
tobiaszpiwowarczyk <tobiasz_piwowarczyk@outlook.com>
2018-08-01 11:47:54 +0200

    - separated theme styling into files

--- frontend/src/app/components/input/directives/input-compact.directive.ts
+++ frontend/src/app/components/input/directives/input-compact.directive.ts
@@ -1,4 +1,4 @@
-import { InputComponent } from './input.component';
+import { InputComponent } from '../input.component';
 import { Directive } from '@angular/core';
 
 @Directive({
--- frontend/src/app/components/input/input.module.ts
+++ frontend/src/app/components/input/input.module.ts
@@ -1,9 +1,9 @@
-import { InputCompactDirective } from './input-compact.directive';
+import { InputCompactDirective } from './directives/input-compact.directive';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { NgModule } from "@angular/core";
 import { InputComponent } from "./input.component";
-import { InputFluidDirective } from './input-fluid.directive';
+import { InputFluidDirective } from './directives/input-fluid.directive';
 
 
 @NgModule({
